a9b80d63190ecf41ffd7d9cdcad28a4f12021632
DomiBot <46618732+DomiBot@users.noreply.github.com>
2019-01-15 00:01:23 -0500

    Add files via upload

---
+++ Notification/Notification_StreamlabsSystem.py
@@ -0,0 +1,87 @@
+import clr
+import sys
+import json
+import os
+import ctypes
+import codecs
+
+ScriptName = "Notification"
+Website = "https://github.com/DomiBot/ScriptChatBot"
+Description = "Notification pour Streamlabs ChatBot"
+Creator = "Domi"
+Version = "1.0.0"
+
+configFile = "config.json"
+settings = {}
+volume = 0.1
+command = "__WelcomeAnnouncer__"
+soundspath = ""
+sounds = []
+
+
+def ScriptToggled(state):
+	return
+
+def Init():
+	global sounds, soundspath, volume, settings
+
+	path = os.path.dirname(__file__)
+	soundspath = path + "\\sounds"
+
+	try:
+		with codecs.open(os.path.join(path, configFile), encoding='utf-8-sig', mode='r') as file:
+			settings = json.load(file, encoding='utf-8-sig')
+	except:
+		settings = {
+			"liveOnly": True,
+			"permission": "Everyone",
+			"volume": 50.0,
+			"useCooldown": False,
+			"useCooldownMessages": False,
+			"userCooldown": 1800,
+			"onUserCooldown": "$user, la commande est sur cooldown pour $cd minutes!"
+		}
+
+	volume = settings["volume"] / 100.0
+	sounds = os.listdir(soundspath)	
+
+    
+	return
+
+def Execute(data):
+	if data.IsChatMessage() and Parent.HasPermission(data.User, settings["permission"], "") and ((settings["liveOnly"] and Parent.IsLive()) or (not settings["liveOnly"])):
+		outputMessage = ""
+		userId = data.User			
+		username = data.UserName
+
+		if settings["useCooldown"] and (Parent.IsOnUserCooldown(ScriptName, command, userId)):
+			if settings["useCooldownMessages"]:
+				cdi = Parent.GetUserCooldownDuration(ScriptName, command, userId)
+				cd = str(cdi / 60) + ":" + str(cdi % 60).zfill(2) 
+				outputMessage = settings["onUserCooldown"]
+				outputMessage = outputMessage.replace("$cd", cd)
+			else:
+				outputMessage = ""
+		else:
+			sound = sounds[Parent.GetRandom(0, len(sounds))]
+
+			soundpath = soundspath + "\\" + sound
+			if userId != "UCRpXO8EWG2Qk6t5HVaXq7tA":
+				if Parent.PlaySound(soundpath, volume):			
+					if settings["useCooldown"]:
+						Parent.AddUserCooldown(ScriptName, command, userId, settings["userCooldown"])
+						
+		outputMessage = outputMessage.replace("$user", username)
+
+		Parent.SendStreamMessage(outputMessage)
+	return
+
+def ReloadSettings(jsonData):
+	Init()
+
+	return
+
+
+
+def Tick():
+	return
