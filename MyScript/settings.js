3590410cba5431ae9a8e8e4f23e4d2c3b6496c27
DomiBot <46618732+DomiBot@users.noreply.github.com>
2019-01-16 06:30:18 -0500

    Add files via upload

---
+++ MyScript/settings.js
@@ -0,0 +1,17 @@
+﻿var settings = {
+  "liveOnlyAnnonceur": true,
+  "liveOnlyNotification": true,
+  "annonceur": true,
+  "notification": true,
+  "permission": "Everyone",
+  "volume": 100,
+  "useCooldown": false,
+  "useCooldownMessages": false,
+  "userCooldown": 300,
+  "onUserCooldown": "$user, la commande est sur cooldown pour $cd minutes!",
+  "SendMessages": true,
+  "DeleteMessage": 0,
+  "AddMessage": "",
+  "ViewerToDelete": "",
+  "SafeQuestion": false
+};
